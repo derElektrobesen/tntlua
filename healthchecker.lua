8720d44677e5a4c0f70b435c949193740ad4ad40
Pavel Berezhnoy <p.berezhnoy@corp.mail.ru>
2016-12-26 20:19:36 +0300

    haproxy healthchecker

---
+++ healthchecker.lua
@@ -0,0 +1,26 @@
+-- This module should be used to check tarantool 1.6 (and older) availability.
+
+local socket = require 'socket'
+local json = require 'json'
+
+-- healthcheck will be called any time when new connection is established
+-- Connection will be closed when data will be sent
+local function healthcheck(s)
+	s:send(json.encode({
+		read_only = box.cfg.read_only,
+	}))
+end
+
+local server
+
+-- run_healthchecker is used to start healthcheck listener.
+function run_healthchecker(host, port)
+	server = socket.tcp_server(host, port, healthcheck)
+	if server == nil or server:error() ~= nil then
+		str = "Can't start healthchecker"
+		if server ~= nil then
+			str = str + ": " + server:error()
+		end
+		box.error(box.error.PROC_LUA, str)
+	end
+end
